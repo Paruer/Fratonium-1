86a4bc06516618ea3270082b4831100ade65c84e
Michael Parker <parker.michael@gmail.com>
2019-08-30 16:19:16 -0500

    Added basic code to link to UI elements, modify them, and respond to button presses.

--- Fratonium/ViewController.swift
+++ Fratonium/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: UI Properties
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    //MARK: Programmatic properties
+    var score: Int = 0
+    
+    //MARK: ViewController functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        categoryLabel.text = "Physical"
+        firstButton.setTitle("No problems. Physically in shape", for: .normal)
+    }
+    
+    //MARK: UI actions
+    @IBAction func questionSelected(sender: UIButton) {
+        if sender == firstButton {
+            addToScore(1)
+        } else if sender == secondButton {
+            addToScore(3)
+        } else if sender == thirdButton {
+            addToScore(5)
+        }
+        
+    }
+    
+    //MARK: Helper functions
+    func addToScore(_ value: Int) {
+        score += value
+        scoreLabel.text = String(score)
     }
 
 
