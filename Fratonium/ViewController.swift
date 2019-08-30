6c5ea4d24b09cfa909b4975c1296848b2dceef7d
Michael Parker <parker.michael@gmail.com>
2019-08-30 14:34:07 -0500

    Question scene built in Interface Builder.  No code.

---
+++ Fratonium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fratonium
+//
+//  Created by Michael Parker on 8/30/19.
+//  Copyright © 2019 Michael Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
